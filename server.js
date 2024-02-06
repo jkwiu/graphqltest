339df905a4d0fe15cfbdd9b972c7468e40d3f5a0
jk <jkwiu2955@gmail.com>
2024-02-07 00:06:14 +0900

    c

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
-import { ApolloServer } from "apollo-server";
+import { ApolloServer, gql } from "apollo-server";
 
-const server = new ApolloServer({});
+const typeDefs = gql`
+  type User {
+    id: ID
+    username: String
+  }
+  type Tweet {
+    id: ID
+    text: String
+    author: User
+  }
+  type Query {
+    allTweets: [Tweet]
+    tweet(id: ID): Tweet
+  }
+`;
+const server = new ApolloServer({ typeDefs });
 
-server.listen().then(({ url }) => console.logs);
+server.listen().then(({ url }) => {
+  console.log(`Running on ${url}`);
+});
